40b5287eb9360c025c9ed9b61e71a29baf806461
CR112-uni <coen.ryan@griffithuni.edu.au>
2024-07-25 09:13:03 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
 console.log("Hello World!");
-
-// Function to add two numbers 
-var addTwoNumbers = (x, y) => { return (x + y) };
-var sum = addTwoNumbers(5, 10);
-
-// Output sum 
-console.log("Sum =", sum)
